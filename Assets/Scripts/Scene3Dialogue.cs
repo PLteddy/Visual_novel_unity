56d0690234fecdc03f64d4089a62fc94a37b6401
VictEmma <victor-emmanuel.ndayishimiye@etu.univ-smb.fr>
2025-03-21 10:04:16 +0100

    UPDATE SCENE3GARAGE
    J'ai arrangé les choix et le fait qu'on puisse cliquer plusieurs fois les boutons. Le fait que j'avais rajouté une salle sur le pc de Mélina n'avait pas fonctionné donc je le refais ici je vérifierais sur mon pc si ça le fait sinon tout le monde devra le faire à son tour pour éviter les bugs

--- Assets/Scripts/Scene3Dialogue.cs
+++ Assets/Scripts/Scene3Dialogue.cs
@@ -13,18 +13,19 @@ public class Scene3Dialogue : MonoBehaviour
     public GameObject ArtBG1;
     public GameObject Rond1;
     public GameObject Rond2;
-    //public GameObject Rond3;
+    public GameObject Rond3;
     public GameObject Rond4;
     public GameObject Choice1a;
     public GameObject Choice1b;
-    //public GameObject Choice2a;
-    //public GameObject Choice2b;
+    public GameObject Choice2a;
+    public GameObject Choice2b;
     public GameObject nextButton;
     private bool allowSpace = true;
 
     private bool firstTime; // nouvelle variable pour simplifier
     private bool hasStartedDialogue = false;
     private bool hasStartedRond2 = false;
+    private bool hasStartedRond3 = false;
 
     void Start()
 {
@@ -36,12 +37,12 @@ public class Scene3Dialogue : MonoBehaviour
     ArtBG1.SetActive(true);
     Rond1.SetActive(false);
     Rond2.SetActive(false);
-    //Rond3.SetActive(false);
+    Rond3.SetActive(false);
     Rond4.SetActive(false);
     Choice1a.SetActive(false);
     Choice1b.SetActive(false);
-    //Choice2a.SetActive(false);
-    //Choice2b.SetActive(false);
+    Choice2a.SetActive(false);
+    Choice2b.SetActive(false);
     nextButton.SetActive(true);
 
     firstTime = PlayerPrefs.GetInt("SceneGarageFirstTime", 0) == 0;
@@ -207,19 +208,142 @@ else if (primeInt == 97){
 }
 
 
+//Rond3_d
 
-// Si on est déjà passé     AVANT DE DECOMMENTEZ IL FAUT VERIFIER SI LA VARIABLE MARCHE BIEN LE BOOLEAN 
-//parce que ça semble pas marcher mon dialogue s'arrête au premier texte
-// else if (primeInt == 199){ 
-//     Char1name.text = "You";
-//     Char1speech.text = "Je suis déjà passé là";
-//     primeInt++;
-    
-// }
-// else if (primeInt == 200){
-//     EndDialogue();
-// }
+        if (primeInt == 120)
+        {
+            ArtChar1a.SetActive(true);
+            Char1name.text = "You";
+            Char1speech.text = "C'est la porte du garage";
+            primeInt++;
+        }
+        else if (primeInt == 121)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "Je pourrais peut être l'ouvrir et m'echapper";
+            primeInt++;
+        }
+        else if (primeInt == 122)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "avec un peu de force ";
+            primeInt++;
+        }
 
+else if (primeInt == 123){
+    Char1name.text = "Jeda";
+    Char1speech.text = "Je tente ou pas ?";
+    nextButton.SetActive(false);
+    allowSpace = false;
+    Choice2a.SetActive(true);
+    Choice2b.SetActive(true);
+}
+
+// after choice 2a
+else if (primeInt == 124){
+    Char1name.text = "YOU";
+    Char1speech.text = "choix1 porte";
+    primeInt = 125; // ira à 125 sur "Next"
+}
+else if (primeInt == 125){
+    Char1name.text = "You";
+    Char1speech.text = "Je suis le choix après 1";
+    primeInt++;
+}
+else if (primeInt == 126){
+    Char1name.text = "You";
+    Char1speech.text = "On parle et parlote";
+    primeInt++;
+}
+else if (primeInt == 127){
+    EndDialogue();
+}
+
+// after choice 2b
+else if (primeInt == 144){
+    Char1name.text = "YOU";
+    Char1speech.text = "choix2 porte";
+    primeInt = 145; // ira à 95 sur "Next"
+}
+else if (primeInt == 145){
+    Char1name.text = "You";
+    Char1speech.text = "Je suis le choix après 2";
+    primeInt++;
+}
+else if (primeInt == 146){
+    Char1name.text = "You";
+    Char1speech.text = "on parle et parlotte toujours";
+    primeInt++;
+}
+else if (primeInt == 147){
+    EndDialogue();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Quand on rappuie sur les boutons ça fait ça 
+        if (primeInt == 199)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Je suis déjà passé par là.";
+            primeInt++;
+        }
+        else if (primeInt == 200)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Je ne vais pas perdre mon temps ici.";
+            primeInt++;
+        }
+        else if (primeInt == 201)
+        {
+            EndDialogue();
+        }
+        if (primeInt == 299)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Je suis déjà passé par là.";
+            primeInt++;
+        }
+        else if (primeInt == 300)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Je ne vais pas perdre mon temps ici.";
+            primeInt++;
+        }
+        else if (primeInt == 301)
+        {
+            EndDialogue();
+        }
+
+
+        //rond3
+                if (primeInt == 399)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Je suis déjà passé par là.";
+            primeInt++;
+        }
+        else if (primeInt == 400)
+        {
+            Char1name.text = "You";
+            Char1speech.text = "Je ne vais pas perdre mon temps ici.";
+            primeInt++;
+        }
+        else if (primeInt == 401)
+        {
+            EndDialogue();
+        }
+
+ 
 
     }
 
@@ -233,7 +357,7 @@ else if (primeInt == 97){
         ArtChar1a.SetActive(false);
         Rond1.SetActive(true);
         Rond2.SetActive(true);
-        //Rond3.SetActive(true);
+        Rond3.SetActive(true);
         Rond4.SetActive(true);
     }
 
@@ -244,12 +368,32 @@ public void Rond1_d()
         hasStartedDialogue = true;
         primeInt = 40;
 
+        // Désactiver les boutons ronds pour éviter qu'ils restent affichés
+        Rond1.SetActive(false);
+        Rond2.SetActive(false);
+        Rond3.SetActive(false);
+        Rond4.SetActive(false);
         DialogueDisplay.SetActive(true);
         nextButton.SetActive(true);
         allowSpace = true;
         Next(); // On lance le Next direct
     }
+    else if (hasStartedDialogue) // Si déjà visité
+    {
+        primeInt = 199; 
+        Rond1.SetActive(false);
+        Rond2.SetActive(false);
+        Rond3.SetActive(false);
+        Rond4.SetActive(false);
+        DialogueDisplay.SetActive(true);
+        nextButton.SetActive(true);
+        allowSpace = true;
+        Next(); // Affiche "Je suis déjà passé par là"
+    }
 }
+
+
+
 
 public void Rond2_d()
 {
@@ -258,21 +402,104 @@ public void Rond2_d()
         hasStartedRond2 = true;
         primeInt = 80;
 
+        // Désactiver les boutons ronds pour éviter qu'ils restent affichés
+        Rond1.SetActive(false);
+        Rond2.SetActive(false);
+        Rond3.SetActive(false);
+        Rond4.SetActive(false);
+
         // Active le dialogue et les contrôles
         DialogueDisplay.SetActive(true);
         nextButton.SetActive(true);
         allowSpace = true;
         Next(); // On lance le Next direct
     }
-    // else  voir211 pour plus d'informations
-    // { 
-    //     primeInt = 199;
-    //     DialogueDisplay.SetActive(true);
-    //     nextButton.SetActive(true);
-    //     allowSpace = true;
-    //     Next(); // On lance le Next direct
-    // }
+    else if (hasStartedRond2) // Si déjà visité
+    {
+        primeInt = 299; 
+
+        Rond1.SetActive(false);
+        Rond2.SetActive(false);
+        Rond3.SetActive(false);
+        Rond4.SetActive(false);
+        DialogueDisplay.SetActive(true);
+        nextButton.SetActive(true);
+        allowSpace = true;
+        Next(); // Affiche "Je suis déjà passé par là"
+    }
 }
+
+
+
+public void Rond3_d()
+{
+    if (!hasStartedRond3)
+    {
+        hasStartedRond3 = true;
+        primeInt = 120;
+
+        // Désactiver les boutons ronds pour éviter qu'ils restent affichés
+        Rond1.SetActive(false);
+        Rond2.SetActive(false);
+        Rond3.SetActive(false);
+        Rond4.SetActive(false);
+
+        // Active le dialogue et les contrôles
+        DialogueDisplay.SetActive(true);
+        nextButton.SetActive(true);
+        allowSpace = true;
+        Next(); // On lance le Next direct
+    }
+    else if (hasStartedRond3) // Si déjà visité
+    {
+        primeInt = 399; 
+
+        Rond1.SetActive(false);
+        Rond2.SetActive(false);
+        Rond3.SetActive(false);
+        Rond4.SetActive(false);
+        DialogueDisplay.SetActive(true);
+        nextButton.SetActive(true);
+        allowSpace = true;
+        Next(); // Affiche "Je suis déjà passé par là"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
 public void Choice1aFunct(){
@@ -291,11 +518,36 @@ public void Choice1bFunct(){
     allowSpace = true;
 }
 
+public void Choice2aFunct(){
+    primeInt = 124;
+    Choice2a.SetActive(false);
+    Choice2b.SetActive(false);
+    nextButton.SetActive(true);
+    allowSpace = true;
+}
+
+public void Choice2bFunct(){
+    primeInt = 144;
+    Choice2a.SetActive(false);
+    Choice2b.SetActive(false);
+    nextButton.SetActive(true);
+    allowSpace = true;
+}
+
+
+
+
+
+
+
+
+
     
         public void Rond4_d()
         {
             SceneManager.LoadScene("SceneCouloir");
         }
 
-    }   
+}
+    
 
